2c43f24e86f05601949a194b71ee5cee50e94f30
mahdiivan <56740285+mahdiivan@users.noreply.github.com>
2025-08-01 03:58:16 -0400

    remove redundant postcss.config.mjs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
-export default {
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./src/app/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
   plugins: [],
